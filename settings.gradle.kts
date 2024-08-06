de5a31748826dcfa03afee6fc92ef93fc4173cef
nikhil <nikhil.pednekar@truemeds.in>
2024-08-06 12:50:03 +0530

    pilot commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "TmComponentKmp"
 include(":shared")
+
+//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
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
     alias(libs.plugins.compose.compiler)
     id("org.jetbrains.compose") version "1.6.11"
     id("maven-publish")
-//apply { "maven-publish" }
-
 }
 
 kotlin {
@@ -16,7 +14,7 @@ kotlin {
             }
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -52,5 +50,56 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["kotlin"])
+
+            groupId = "com.intellihealth"
+            artifactId = "shared-release"
+            version = "0.1.0"
+            artifact("$buildDir/outputs/aar/shared-release.aar")
+
+
+//            pom {
+//                name.set("TMComponentKmp")
+//                description.set("A Kotlin Multiplatform library for IntelliHealth TrueMeds")
+//                url.set("http://www.intellihealth.com/shared-release")
+//
+//                licenses {
+//                    license {
+//                        name.set("The Apache License, Version 2.0")
+//                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+//                    }
+//                }
+//
+//                developers {
+//                    developer {
+//                        id.set("intellihealth")
+//                        name.set("IntelliHealth Team")
+//                        email.set("support@intellihealth.com")
+//                    }
+//                }
+//
+//                scm {
+//                    connection.set("scm:git:git://github.com/intellihealth/shared-release.git")
+//                    developerConnection.set("scm:git:ssh://github.com:intellihealth/shared-release.git")
+//                    url.set("http://github.com/intellihealth/shared-release")
+//                }
+//            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://mavan.pkg.github.com/nikhilpednekar1/TmComponentKmp")
+            credentials {
+                username = "nikhilpednekar1"//project.findProperty("repoUser") as String? ?: ""
+                password = "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"//project.findProperty("repoPassword") as String? ?: ""
+            }
+        }
     }
 }
